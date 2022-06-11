04a68968bf985e715078d4c009ab3e39b2786b48
Nguyễn Minh Hoàng <53693390+minhhoang-00@users.noreply.github.com>
2022-06-12 00:10:33 +0700

    sdsdd

--- main/urls.py
+++ main/urls.py
@@ -1,4 +1,4 @@
-"""main URL Configuration
+"""DoAnTotNghiep URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from user.views import GetAllUser
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('getuser/', GetAllUser.as_view()),
 ]
